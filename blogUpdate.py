a546881a8a4a616e98702837862aab2396ce8eb1
voitgxd <1316110738@qq.com>
2015-11-23 11:00:58 +0800

    backup

--- blogUpdate.py
+++ blogUpdate.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 		os.system("hexo clean & (hexo generate) & (hexo deploy)")
 		inputCo = input('backup source to git? y/n : ')
 		if inputCo == "y":
-			os.system("git add .")
+			os.system("git add -A")
 			os.system('git commit -m "backup"')
 			os.system("git push origin master")
 		else:
